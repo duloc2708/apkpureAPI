ac07bfde70b0bdc122ac643f2d9f63e547cb6928
Loc Du <locdu@kolabs.co>
2019-02-19 11:46:35 +0700

    update code

--- app.js
+++ app.js
@@ -25,7 +25,7 @@
 // no matter where we actually lift from.
 // > Note: This is not required in order to lift, but it is a convenient default.
 process.chdir(__dirname);
-//test
+//test 1332
 
 
 // Attempt to import `sails` dependency, as well as `rc` (for loading `.sailsrc` files).
