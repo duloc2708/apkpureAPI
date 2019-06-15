d86acfc1b9bbc60e447ce61797db6e56db88bb9b
Loc Du <locdu@kolabs.co>
2019-06-15 12:58:15 +0700

    update

---
+++ serverRQAppGame.js
@@ -0,0 +1,21 @@
+//import { setTimeout } from 'timers';
+
+var express = require('express');
+var app = express();
+
+var job = require('node-cron');
+var i = 1;
+
+job.schedule('* * * * *', async function () {
+  console.log('request/rqGame>>>>>');
+  let getGame = require('./request/requestGame.js');
+  // let getGame = require('./request/requestGameManual.js');
+
+  let rqGame = await getGame.Data();
+});
+;
+
+
+
+
+app.listen(3009);
